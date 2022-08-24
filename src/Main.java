e3fd5213342343c33230f13c05e751facacdd63e
sarai-surpreet <sursar02@my.yorku.ca>
2022-08-24 14:56:26 -0400

    Fixed randomlySelectLyrics() method to not select the same lyrics twice. Albeit the implementation is not that strong because Random has may have to run multiple times in order to select a lyric that hasn't been selected before. This may be inefficient especially when closer to the end of the game where most lyrics have been selected and when there are a lot of songs stored. May need to come up with a better implementation for this method. Besides that, game is functional and correct. Just that the code is a bit messy...

--- src/Main.java
+++ src/Main.java
@@ -8,32 +8,42 @@ public class Main {
         Scanner scan = new Scanner(System.in);
 
         while (playAgain.equalsIgnoreCase("y")) {
-            System.out.println("Welcome to Music Quiz!");
-            System.out.println("Here are your instructions: ");
+            System.out.println("***** Welcome to Music Quiz! ******");
+            System.out.println("Here are your instructions:");
+            System.out.println("A randomly generated lyrics from a song will be displayed for you to guess which song it is from. There are 5 rounds to play through.\n" +
+                    "If you do not want to keep playing, simply input \"q\" to leave the game. After 5 rounds or after you choose to quit, you can choose to play again.\n" +
+                    "Let's Start!\n");
+
             SongCollection sc = new SongCollection();
 
             String answer = "";
-            sc.addSongs();
 
-            //    sc.displayCollection();
-            while (sc.numberOfRoundsPlayed < 3 && !answer.equals("q")) {
+            while (sc.numberOfRoundsPlayed < 5 && !answer.equals("q")) {
                 sc.randomlySelectLyrics();
                 sc.displayRandomLyrics();
 
                 System.out.println("What's your guess?");
                 answer = scan.nextLine();
 
-                sc.displayCorrectAnswer(answer);
-                if (!answer.equals("q")) {
+                if(answer.equals("q")){
+                    System.out.println("You have chosen to end the game!\n");
+
+                }
+                else{
+                    sc.displayCorrectAnswer(answer);
                     sc.displayUserScore();
                 }
+
+
+
 
             }
 
             System.out.println("Would you like to play again? (y/n) ");
             String play = scan.nextLine();
-            if(play.equalsIgnoreCase("n")){
+            if(!play.equalsIgnoreCase("y")){
                 playAgain = "n";
+
             }
 
 
--- src/SongCollection.java
+++ src/SongCollection.java
@@ -1,9 +1,9 @@
+import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Random;
 
 /*
 Control the song collection operations:
-    - adding keys and values into hashtable  X
     - randomize lyrics method X
     - display lyrics to Main class X
     - check User guess X
@@ -11,29 +11,18 @@ Control the song collection operations:
  */
 public class SongCollection {
     // Keys are song names & values are lyrics
-    // Hashtable may not be needed for this problem
-    public Hashtable<String, String> songCollection = new Hashtable<>();
-    String[] songsNames = {"Sacrifice", "Take My Breath", "Out of Time"};
-    String [] lyrics = {"I was born in a city where the winter nights never sleep.", "It's like a dream what she feels with me. She loves to be on the edge.",
-            "If he mess up just a little, baby, you know my line. If you don't trust him a little, then come right back, girl, come right back."};
+    Hashtable <String,String> songCollection = new Hashtable<>();
+    String[] songsNames = {"Sacrifice", "Take My Breath", "Out of Time", "Faith", "In Your Eyes", "Starry Eyes"};
+    String [] lyrics = {"\"I was born in a city where the winter nights never sleep.\"", "\"It's like a dream what she feels with me. She loves to be on the edge.\"",
+            "\"If he mess up just a little, baby, you know my line. If you don't trust him a little, then come right back, girl, come right back.\"",
+            "\"I feel everything, I feel everything from my body to my soul\"", "\"You always try to hide the pain, You always know just what to say.\"",
+            "\"And I wanna feel you close. But you're defeated, baby. Broken, hurtin', sufferin' from a shattered soul.\""};
+
     String randomLyrics;
-    int userscore =0;
+    int userScore =0;
+    ArrayList<String> alreadySelectedLyrics = new ArrayList<>();
     int numberOfRoundsPlayed=0;
-    boolean endGame;
 
-    // Add songs names and their corresponding lyrics into Hashtable
-    public void addSongs(){
-        for(int i=0; i < songsNames.length; i++){
-            songCollection.put(songsNames[i], lyrics[i]);
-        }
-
-    }
-    // For testing that values were inputted into hashtable correctly
-    public void displayCollection(){
-       for(String song: songCollection.keySet()){
-           System.out.println("Song:" + song + "\nLyrics: " +  songCollection.get(song));
-        }
-    }
 
     /*
     Use Random class:
@@ -45,13 +34,30 @@ public class SongCollection {
         Random ran = new Random();
         int randNum = ran.nextInt(lyrics.length);
 
+        //if the lyrics already exists in alreadySelectedLyrics, rerun the randomizer
+        // implementation is kinda trash cuz the randomizer has to run multiple times then until
+        // it selects a number that has not been selected before
+        // maybe it would be more efficient if i turned the lyrics array to an array list and deleted the songs that have already been selected.
+        // then at the end of the game, re-add the lyrics?
+        boolean alreadyExists = false;
+        while(!alreadyExists){
+            if(alreadySelectedLyrics.contains(lyrics[randNum])){
+                randNum = ran.nextInt(lyrics.length);
+            }
+            else{
+                alreadyExists= true;
+            }
+        }
+
+
         randomLyrics = lyrics[randNum];
+        alreadySelectedLyrics.add(lyrics[randNum]);
 
         return randomLyrics;
     }
 
     public void displayRandomLyrics(){
-        System.out.println(numberOfRoundsPlayed+1 + ". " + randomLyrics);
+        System.out.println(numberOfRoundsPlayed+1 + "." + randomLyrics);
         numberOfRoundsPlayed++;
     }
 
@@ -60,7 +66,7 @@ public class SongCollection {
         for(int i=0; !check && i< songsNames.length; i++){
             if(guess.equalsIgnoreCase(songsNames[i])){
                 check = true;
-                userscore++;
+                userScore++;
             }
         }
 
@@ -69,11 +75,7 @@ public class SongCollection {
 
     public void displayCorrectAnswer(String guess) {
         if (checkUserGuess(guess)) {
-            System.out.println("Yup that's right!");
-        } else if (guess.equalsIgnoreCase("q")) {
-            endGame = true;
-            System.out.println("You have chosen to end the game.");
-
+            System.out.println("Yup that's right!\n");
         }
         else{
             System.out.println("Sorry this is actually: ");
@@ -91,7 +93,11 @@ public class SongCollection {
     }
 
     public void displayUserScore(){
-        System.out.println("Your current score is: " + userscore + " out of 5");
+        System.out.println("Your score is: " + userScore + " out of 5");
+        if(userScore==5){
+            System.out.println("You got perfect! Good job!!");
+        }
+        System.out.print("\n");
     }
 
 
